883aa11b284e430e9a865ecf245ab990b338ad4a
Rabih-Nadr <rabih.nadr@gmail.com>
2023-05-06 10:06:13 -0400

    HomePage created

---
+++ src/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to my portfolio!</h1>
+        <p>My name is <b>Rabih Nader</b>, and I'm a web developer with 3 years of experience.</p>
+        <button>Contact Me</button>
+      </div>
+    );
+  }
+  
+
+  export default HomePage;
