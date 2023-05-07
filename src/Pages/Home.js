daa9595039a46d04dc43ee487dee9425278cbf4c
Rabih-Nadr <rabih.nadr@gmail.com>
2023-05-07 09:54:42 -0400

    CSS added to the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,22 @@
+// Home.js
+
 import React from "react";
 import "../styleSheet/Home.css";
-import profilePhoto from "../assets/profile-photo.png";
 
 function Home() {
   return (
     <div className="homepage-container">
-      <img className="homepage-photo" src={profilePhoto} alt="Profile" />
-      <h1 className="homepage-name">Rabih Nader</h1>
-      <h2 className="homepage-headline">Web Developer</h2>
-      <div className="homepage-section">
-        <h3 className="homepage-section-title">Skills</h3>
-        <p className="homepage-section-content">React, Node.js, CSS, HTML, etc.</p>
+      <div className="content">
+        <div className="profile-photo"></div>
+        <div className="bio">
+        <p>Summary of my skills and achievements.</p>
+        <h2>Web Developer</h2>
+        <h1>Rabih Nader</h1>
+        </div>
       </div>
-      <div className="homepage-section">
-        <h3 className="homepage-section-title">Achievements</h3>
-        <p className="homepage-section-content">Built a successful e-commerce website for a local business.</p>
-      </div>
-      <button className="homepage-button">Contact Me</button>
     </div>
   );
 }
 
 export default Home;
+
