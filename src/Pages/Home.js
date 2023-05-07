60a907da636fdebc5e4aa691dc96e4927900707f
Rabih-Nadr <rabih.nadr@gmail.com>
2023-05-07 09:05:49 -0400

    Home page css is created

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,24 @@
-//HomePage.js
-
 import React from "react";
+import "../styleSheet/Home.css";
+import profilePhoto from "../assets/profile-photo.png";
 
-function HomePage() {
-    return (
-      <div>
-        <h1>Welcome to my portfolio!</h1>
-        <p>My name is <b>Rabih Nader</b>, and I'm a web developer with 3 years of experience.</p>
-        <button>Contact Me</button>
+function Home() {
+  return (
+    <div className="homepage-container">
+      <img className="homepage-photo" src={profilePhoto} alt="Profile" />
+      <h1 className="homepage-name">Rabih Nader</h1>
+      <h2 className="homepage-headline">Web Developer</h2>
+      <div className="homepage-section">
+        <h3 className="homepage-section-title">Skills</h3>
+        <p className="homepage-section-content">React, Node.js, CSS, HTML, etc.</p>
       </div>
-    );
-  }
-  
+      <div className="homepage-section">
+        <h3 className="homepage-section-title">Achievements</h3>
+        <p className="homepage-section-content">Built a successful e-commerce website for a local business.</p>
+      </div>
+      <button className="homepage-button">Contact Me</button>
+    </div>
+  );
+}
 
-  export default HomePage;
+export default Home;
